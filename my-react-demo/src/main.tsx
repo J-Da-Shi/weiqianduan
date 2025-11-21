35194d0d1638ce4a4474395391c8571109e2434d
shida <v-shida1@xiaomi.com>
2025-11-21 19:01:50 +0800

    给react子应用增加一些大文件，给子应用增加打包后的体积，模拟打包内容很多，影响页面首屏加载

--- my-react-demo/src/main.tsx
+++ my-react-demo/src/main.tsx
@@ -4,10 +4,21 @@ import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import largeFile from './largeFile.ts'
+import LargeFile2 from './a.ts'
+import LargeFile3 from './a copy.ts'
+import LargeFile4 from './a copy 2.ts'
+import LargeFile5 from './a copy 3.ts'
+import LargeFile6 from './a copy 4.ts'
+import LargeFile7 from './a copy 5.ts'
+import LargeFile8 from './a copy 6.ts'
+import LargeFile9 from './a copy 7.ts'
+import LargeFile10 from './a copy 8.ts'
+import LargeFile11 from './a copy 9.ts'
 
 const root = createRoot(document.getElementById('root')!)
 
-
+console.log('largeFile.js',LargeFile2, LargeFile3, LargeFile4, LargeFile5, LargeFile6, LargeFile7, LargeFile8, LargeFile9, LargeFile10, LargeFile11, largeFile);
 function render(props: any) {
   root.render(
     <StrictMode>
